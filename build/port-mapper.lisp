3b8fb1240c0cf4206e7229c2a889adc2b299df44
Max Rottenkolber <max@mr.gy>
2016-09-21 23:47:23 +0200

    bin/erlangen-port-mapper: add command line arguments, omit reverted ROOT.

--- build/port-mapper.lisp
+++ build/port-mapper.lisp
@@ -4,16 +4,29 @@
 
 (asdf:load-system :erlangen)
 
+(defun port-mapper ()
+  (when (intersection *command-line-argument-list*
+                      '("-h" "-help" "--help")
+                      :test 'string=)
+    (princ "Usage: erlangen-port-mapper [host] [registry-port] [directory-port]
+       erlangen-port-mapper -h|-help|--help")
+    (quit 0))
+  (destructuring-bind (exe &optional host rport dport)
+      *command-line-argument-list*
+    (declare (ignore exe))
+    (apply 'erlangen.distribution.protocol.port-mapper:port-mapper
+           (append (when host
+                     `(:registry-host ,host :directory-host ,host))
+                   (when rport
+                     `(:registry-port ,(parse-integer rport)))
+                   (when dport
+                     `(:directory-port ,(parse-integer dport)))))))
+
 (gc)
 
 (save-application
  "bin/erlangen-port-mapper"
- :toplevel-function
- (lambda ()
-   (erlangen:root ()
-     (erlangen.distribution.protocol.port-mapper:port-mapper)))
+ :toplevel-function 'port-mapper
  :error-handler :quit
  :purify t
  :prepend-kernel t)
-
-(quit 0)
