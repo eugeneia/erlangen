90c714ad5570074afb4a8a21c6e21397be252bb9
Max Rottenkolber <max@mr.gy>
2016-12-08 23:51:04 +0100

    CCL: Add PRINT-OBJECT method for SOCKET-ERROR condition.

--- ccl.lisp
+++ ccl.lisp
@@ -8,3 +8,10 @@ otherwise."
   (%wait-on-semaphore-ptr (semaphore-value s) 0 0 flag))
 
 (export 'try-semaphore)
+
+(defmethod print-object ((o socket-error) stream)
+  (print-unreadable-object (o stream :type t :identity t)
+    (format stream "~a: ~a"
+            (socket-error-situation o)
+            (or (socket-error-identifier o)
+                (socket-error-code o)))))
