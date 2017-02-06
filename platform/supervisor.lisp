eugeneia/erlangen

commit ab7987bf5af73ea659c773fda04f381f066b3350

platform: add logging features to supervisor and timer.

platform/supervisor.lisp
@@ -1,7 +1,7 @@
 ;;;; Generic supervisor agent.
 
 (defpackage erlangen-platform.supervisor
-  (:use :cl :erlangen)
+  (:use :cl :erlangen :erlangen-platform.log)
   (:export :supervisor
            :one-for-one
            :one-for-all
@@ -25,9 +25,15 @@
                  :spawn-args spawn-args)))
 
 (defun start-child (child)
-  (with-slots (function spawn-args agent) child
+  (with-slots (id function spawn-args agent) child
+    (write-log* `(,id :start))
     (setf agent (apply 'spawn function :attach :monitor spawn-args)))
   child)
+
+(defun stop-child (child reason)
+  (with-slots (id agent) child
+    (write-log* `(,id :stop))
+    (exit reason agent)))
 
 (defun restartable-p (child status)
   (case (child-restart child)
@@ -44,20 +50,23 @@
 
 (defun supervisor (childspecs &key (strategy 'one-for-one)
                                    (intensity 1)
-                                   (period 5))
+                                   (period 5)
+                                   log)
   "DOCUMENT ME! I AM PUBLIC!"
   (check-type intensity (integer 0))
   (check-type period (integer 1))
-  (let ((*maximum-intensity* (/ intensity period))
-        (*restarts* 0)
-        (*start* (get-universal-time))
-        (children (loop for childspec in childspecs collect
-                       (start-child (make-child childspec)))))
+  (let* ((*maximum-intensity* (/ intensity period))
+         (*restarts* 0)
+         (*start* (get-universal-time))
+         (*log* log)
+         (children (loop for childspec in childspecs collect
+                        (start-child (make-child childspec)))))
     (loop for notice = (receive) do
          (destructuring-bind (agent status &rest values) notice
-           (declare (ignore values))
            (let ((child (find agent children :key 'child-agent)))
-             (when (and child (restartable-p child status))
+             (check-type child child)
+             (write-log* (list* (child-id child) status values))
+             (when (restartable-p child status)
                (check-restart-intensity)
                (funcall strategy children child notice)))))))
 
@@ -68,15 +77,15 @@
 (defun one-for-all (children failed notice)
   (loop for child in children do
        (unless (eq child failed)
-         (exit notice (child-agent child)))
+         (stop-child child notice))
        (start-child child)))
 
 (defun rest-for-one (children failed notice)
   (loop with rest-p = nil
      for child in children
-     when rest-p do
-       (exit notice (child-agent child))
-       (start-child child)
      when (eq child failed) do
        (start-child failed)
-       (setf rest-p t)))
+       (setf rest-p t)
+     when rest-p do
+       (stop-child child notice)
+       (start-child child)))

platform/erlangen-platform.asd
@@ -5,9 +5,9 @@
   "Reusable component framework for Erlangen."
   :author "Max Rottenkolber <max@mr.gy>"
   :license "AGPL-3.0"
-  :components ((:file "supervisor")
-               (:file "logger")
+  :components ((:file "logger")
                (:file "server")
-               (:file "timer")
+               (:file "supervisor" :depends-on ("logger"))
+               (:file "timer" :depends-on ("logger"))
                (:file "socket-server" :depends-on ("supervisor")))
   :depends-on ("erlangen" "trivia"))

platform/timer.lisp
@@ -1,5 +1,5 @@
 (defpackage erlangen-platform.timer
-  (:use :cl :erlangen)
+  (:use :cl :erlangen :erlangen-platform.log)
   (:export :timer))
 
 (in-package :erlangen-platform.timer)
@@ -28,18 +28,19 @@
         ticklist)))
 
 (defun insert-timer (timer tick)
+  (write-log* (list* :set timer))
   (push timer (cdr (find-or-insert-ticklist tick))))
 
 (defun pop-timers (tick)
   (loop while (and (cdr *ticks*) (<= (caadr *ticks*) tick))
-        for (current-tick . timers) = (pop (cdr *ticks*)) do
+        for (start . timers) = (pop (cdr *ticks*)) do
        (loop for timer in timers do
-            (destructuring-bind (event agent &key start repeat)
+            (write-log* (list* :emit tick timer))
+            (destructuring-bind (event agent &key repeat &allow-other-keys)
                 timer
-              (declare (ignore start))
               (ignore-errors (send event agent))
               (when repeat
-                (insert-timer timer (+ current-tick repeat)))))))
+                (insert-timer timer (+ start repeat)))))))
 
 (defun receive-timers (time-function)
   (loop for timer = (if (cdr *ticks*)
@@ -60,10 +61,12 @@
 (defun timer (&key (name :timer)
                    (time-function 'get-universal-time)
                    (sleep-function 'sleep)
-                   (max-sleep 1))
+                   (max-sleep 1)
+                   log)
   (register name)
   (unwind-protect
-       (let ((*ticks* (list 'ticks)))
+       (let ((*ticks* (list 'ticks))
+             (*log* log))
          (loop do
               (pop-timers (funcall time-function))
               (receive-timers time-function)

commit 2a682e44c2bbfe710fcfe49f6d608462f41f56e8

platform/supervisor: fix bug introduced in ab7987b (ignore exit notices from stopped children)

platform/supervisor.lisp
@@ -64,11 +64,11 @@
     (loop for notice = (receive) do
          (destructuring-bind (agent status &rest values) notice
            (let ((child (find agent children :key 'child-agent)))
-             (check-type child child)
-             (write-log* (list* (child-id child) status values))
-             (when (restartable-p child status)
-               (check-restart-intensity)
-               (funcall strategy children child notice)))))))
+             (when child
+               (write-log* (list* (child-id child) status values))
+               (when (restartable-p child status)
+                 (check-restart-intensity)
+                 (funcall strategy children child notice))))))))
 
 (defun one-for-one (children failed notice)
   (declare (ignore children notice))