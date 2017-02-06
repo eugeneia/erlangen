71fc44d32513541495e6b6012ef5efb8ae627e5d
Max Rottenkolber <max@mr.gy>
2017-02-06 02:53:02 +0100

    ccl: tune PRINT-OBJECT of SIMPLE-ERROR and SOCKET-ERROR.

--- ccl.lisp
+++ ccl.lisp
@@ -9,8 +9,13 @@ otherwise."
 
 (export 'try-semaphore)
 
+(defmethod print-object ((o simple-error) stream)
+  (print-unreadable-object (o stream :type t)
+    (with-slots (format-control format-arguments) o
+      (apply 'format stream format-control format-arguments))))
+
 (defmethod print-object ((o socket-error) stream)
-  (print-unreadable-object (o stream :type t :identity t)
+  (print-unreadable-object (o stream :type t)
     (format stream "~a: ~a"
             (socket-error-situation o)
             (or (socket-error-identifier o)
